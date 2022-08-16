926c484997feba24d81758520885821479dbc60f
M Sarmad Qadeer <MSarmadQadeer@gmail.com>
2022-08-16 16:48:18 +0500

    add web functionality

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+const textField = document.querySelector('input[type="text"]');
+const button = document.querySelector('button');
+
+button.addEventListener('click', function() {
+  const text = textField.value;
+  console.log(text);
+  window.location = `/api/${text}`;
+} );
