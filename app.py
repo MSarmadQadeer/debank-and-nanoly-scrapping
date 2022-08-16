8bacb04ddad3147b39254623747032849bd3b1d3
M Sarmad Qadeer <MSarmadQadeer@gmail.com>
2022-08-16 19:10:54 +0500

    change titles

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def index():
-    return render_template("index.html", title="Hello")
+    return render_template("index.html", title="Home Screen")
 
 
 @app.route('/<publicAddress>')
 def apiCall(publicAddress):
     results = getScrappedData(publicAddress)
-    return render_template("index.html", title="API Call", results=results)
+    return render_template("index.html", title="Results Screen", results=results)
 
 
 if __name__ == "__main__":
